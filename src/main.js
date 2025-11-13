f6e427f9db0cf403bc48bdb14380bc5e4de78d0b
Kateryna <katyaukr2014@gmail.com>
2025-11-13 23:18:15 +0200

    fix scroll

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import './js/swipper.js';
 import './js/tab.js';
 import './js/load-more.js';
 import './js/accordion.js';
-import './js/smooth-scroll.js';
 import './js/mobile-menu.js';
 import './js/active-link.js';
+import './js/smooth-scroll.js';
