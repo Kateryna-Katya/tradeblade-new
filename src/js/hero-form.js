ca0c3318e416e5e87af9660c19749f4e3fdf7b7e
Kateryna <katyaukr2014@gmail.com>
2025-11-13 15:38:18 +0200

    add section 4 about

--- src/js/hero-form.js
+++ src/js/hero-form.js
@@ -1,28 +1,35 @@
-document.getElementById('submitBtn').addEventListener('click', function(e) {
-  e.preventDefault();
-  
-  const emailInput = document.getElementById('email');
-  const message = document.querySelector('.form-message');
-  const email = emailInput.value.trim();
-  
-  if (!email) {
-    message.textContent = 'Введите ваш email!';
-    message.style.color = '#FF5C5C';
-    message.style.opacity = 1;
-    return;
-  }
-  
-  message.textContent = 'Отправка...';
-  message.style.color = '#48CFFF';
-  message.style.opacity = 1;
-  
-  setTimeout(() => {
-    message.textContent = 'Спасибо! Форма отправлена✅';
-    message.style.color = '#48CFFF';
- 
+document.addEventListener('DOMContentLoaded', () => {
+  const forms = document.querySelectorAll('.subscribe-form');
+
+  forms.forEach(form => {
+    const emailInput = form.querySelector('.email');
+    const message = form.querySelector('.form-message');
+    const btn = form.querySelector('.submitBtn');
+
+    btn.addEventListener('click', (e) => {
+      e.preventDefault();
+      const email = emailInput.value.trim();
+
+      if (!email) {
+        message.textContent = 'Введите ваш email!';
+        message.style.color = '#FF5C5C';
+        message.style.opacity = 1;
+        return;
+      }
+
+      message.textContent = 'Отправка...';
+      message.style.color = '#48CFFF';
+      message.style.opacity = 1;
+
       setTimeout(() => {
-      emailInput.value = '';
-      message.style.opacity = 0;
-    }, 5000);
-  }, 1500);
+        message.textContent = 'Спасибо! Форма отправлена ✅';
+        message.style.color = '#48CFFF';
+
+        setTimeout(() => {
+          emailInput.value = '';
+          message.style.opacity = 0;
+        }, 5000);
+      }, 1500);
+    });
+  });
 });
